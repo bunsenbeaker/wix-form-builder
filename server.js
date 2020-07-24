83cf131a518fa0e39e0a343c211f2872fc03f825
bunsenbeaker <33667405+bunsenbeaker@users.noreply.github.com>
2020-07-24 11:33:38 +0300

    server minor refactoring

--- server.js
+++ server.js
@@ -1,40 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const apiRoutes = require("./src/server/routes")
 
-const routes = require("./src/server/routes")
+connectDB('mongodb://localhost/formbuilder');
+initServer();
 
-// Initialize the app
-const app = express();
+function connectDB(uri) {
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+       
+    // Added check for DB connection
+    if(!mongoose.connection) {
+        console.log("Error connecting db")
+    }else{
+        console.log("Connected to MongoDB successfully")
+    }
+}
 
+function initServer(){
+    const app = express();
 
-app.use(bodyParser.urlencoded({
-    extended: false
- }));
-app.use(bodyParser.json());
-app.use('/api', routes);
+    app.use(bodyParser.urlencoded({
+        extended: false
+     }));
+    app.use(bodyParser.json());
+    setRoutes(app);
+   
+    const port = process.env.PORT || 8080;
+    app.listen(port , function () {
+         console.log("Running wix-form-builder server on port " + port);
+    });
+}
 
+function setRoutes(app) {
+    app.use('/api', apiRoutes);
+}
 
-dotenv.config();
-
-const uri = `mongodb://localhost/formbuilder`;
-console.log(uri);
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
-var db = mongoose.connection;
-
-// Added check for DB connection
-if(!db)
-    console.log("Error connecting db")
-else
-    console.log("Db connected successfully")
-
-
-// Setup server port
-const port = process.env.PORT || 8080;
-// Send message for default URL
-//app.get('/', (req, res) => res.send('Hello World with Express'));
-// Launch app to listen to specified port
-app.listen(port, function () {
-     console.log("Running FormBuilder Backend on port " + port);
-});
