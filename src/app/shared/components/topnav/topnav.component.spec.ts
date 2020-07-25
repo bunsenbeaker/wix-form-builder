d7d8ceaeccbb52ad0746b275aaa595ea411fdb50
bunsenbeaker <33667405+bunsenbeaker@users.noreply.github.com>
2020-07-25 12:40:17 +0300

    Update topnav.component.spec.ts

--- src/app/shared/components/topnav/topnav.component.spec.ts
+++ src/app/shared/components/topnav/topnav.component.spec.ts
@@ -1,14 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { TopnavComponent } from './topnav.component';
+import { By } from '@angular/platform-browser';
 
-describe('TopnavComponent', () => {
+fdescribe('TopnavComponent', () => {
   let component: TopnavComponent;
   let fixture: ComponentFixture<TopnavComponent>;
+  let store: MockStore;
+  let topnav;
+
+  const initialState = { router: {
+    state: {
+      url: '/'
+    }
+  } };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TopnavComponent ]
+      declarations: [ TopnavComponent ],
+      providers: [provideMockStore({ initialState })]
     })
     .compileComponents();
   }));
@@ -17,9 +28,55 @@ describe('TopnavComponent', () => {
     fixture = TestBed.createComponent(TopnavComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    store = TestBed.inject(MockStore);
+    const topnavdebug =  fixture.debugElement.query(By.css('#topnav'));
+
+    if (topnavdebug) {
+      topnav = topnavdebug.nativeElement;
+    }
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not display topnav on homepage', () => {
+
+    expect(topnav.hasAttribute('hidden')).toEqual(true);
+  });
+
+  it('should display topnav on formslist page', () => {
+
+    store.setState({ router: { state: { url: '/forms'}} });
+    fixture.detectChanges();
+
+    expect(topnav.hasAttribute('hidden')).toEqual(false);
+
+  });
+
+  it('should display topnav on addform page', () => {
+
+    store.setState({ router: { state: { url: '/addform'}} });
+    fixture.detectChanges();
+
+    expect(topnav.hasAttribute('hidden')).toEqual(false);
+
+  });
+
+  it('should display topnav on submissions page', () => {
+
+    store.setState({ router: { state: { url: '/submissions'}} });
+    fixture.detectChanges();
+
+    expect(topnav.hasAttribute('hidden')).toEqual(false);
+  });
+
+
+  it('should display topnav on submissions page', () => {
+
+    store.setState({ router: { state: { url: '/forms/5f1ab656a4248f59ac9528d7'}} });
+    fixture.detectChanges();
+
+    expect(topnav.hasAttribute('hidden')).toEqual(false);
   });
 });
